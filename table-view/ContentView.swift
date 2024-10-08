c9ade16d035d6b2b26b3644c83b57d037dc6881b
annyconfessor <anny.confessor@gmail.com>
2024-10-08 15:54:05 -0300

    Initial Commit

---
+++ table-view/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  table-view
+//
+//  Created by Anny Caroliny on 08/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ table-view/table_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  table_viewApp.swift
+//  table-view
+//
+//  Created by Anny Caroliny on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct table_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
