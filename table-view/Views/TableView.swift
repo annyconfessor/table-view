3280d67c927fa06378af1acae4f2f8ad194f76b6
annyconfessor <anny.confessor@gmail.com>
2024-10-21 17:15:20 -0300

    header stacks added with static image and texts, and the bell icon

--- table-view/Views/TableView.swift
+++ table-view/Views/TableView.swift
@@ -22,12 +22,27 @@ struct TableView: View {
         ZStack(alignment: .top) {
             
             HStack() {
-                Text("Menu icon")
-                Text("Logo")
-                Text("Settings")
-            }.padding(.top, 10)
+                Image("baby-yoda")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    .frame(width: 50, height: 50, alignment: .center)
+                    .clipped()
+                    .clipShape(Circle())
+                VStack(alignment: .leading) {
+                    Text("Good morning,").font(.system(size: 20))
+                    Text("Anny").font(.system(size: 24))
+                }
+                .padding(.leading, 10)
+                Spacer()
+                Image(systemName: "bell.badge")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    .frame(width: 24, height: 24)
+                    .padding(.horizontal, 10)
+            }
             .font(Font.custom("Poppins-Medium", size: 20))
-            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+//            .background(Color.gray)
+            .frame(width: 350, height: 100)
             
             VStack() {
                 ForEach(Array(items.keys), id: \.self) { item in
